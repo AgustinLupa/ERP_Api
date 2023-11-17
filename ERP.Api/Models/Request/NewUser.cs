63944b494bdff9d92651475a469fbea3dac2ca48
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-11-17 17:06:49 -0300

    Agregados request para nuevo usuario y clase de resultados HTTP

---
+++ ERP.Api/Models/Request/NewUser.cs
@@ -0,0 +1,19 @@
+﻿using ERP.Api.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ERP.Api.Models.Request;
+
+public class NewUser
+{
+    [Required(ErrorMessage ="El campo 'usuario' es obligatorio.")]
+    [StringLength(20, MinimumLength = 4, ErrorMessage = "El nombre de usuario debe tener una longitud de entre 4 y 20 caracteres.")]
+    public string username { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "El campo 'contraseña' es obligatorio.")]
+    [StringLength(30, MinimumLength = 8, ErrorMessage = "La contraseña de usuario debe tener una longitud de entre 8 y 30 caracteres.")]
+    public string password { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Debe indicar un rol para el usuario.")]
+    public Roles id_role { get; set; }
+
+}
---
+++ ERP.Api/Models/Response/HttpResult.cs
@@ -0,0 +1,9 @@
+﻿namespace ERP.Api.Models.Response;
+
+public class HttpResult
+{
+    public int StatusCode { get; set; } = 200;
+    public string Message { get; set; } = string.Empty;
+    public object Request { get; set; } = new();
+    public object Response { get; set; } = new();
+}
