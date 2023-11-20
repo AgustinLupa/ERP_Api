34755b3f121286e364eaaacac3afae1e62c22b1a
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2023-11-20 13:20:21 -0300

    Agregada clase de credenciales login y renombrada clase de datos de usuario

--- ERP.Api/Models/Request/SaveUser.cs
+++ ERP.Api/Models/Request/SaveUser.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ERP.Api.Models.Request;
 
-public class NewUser
+public class SaveUser
 {
     [Required(ErrorMessage ="El campo 'usuario' es obligatorio.")]
     [StringLength(20, MinimumLength = 4, ErrorMessage = "El nombre de usuario debe tener una longitud de entre 4 y 20 caracteres.")]
@@ -15,5 +15,6 @@ public class NewUser
 
     [Required(ErrorMessage = "Debe indicar un rol para el usuario.")]
     public Roles id_role { get; set; }
+    public int state { get; set; } = 1;
 
 }
